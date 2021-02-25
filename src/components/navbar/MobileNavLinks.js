44d500dd3ffb549617f558078e76841d99be4a39
jeromeski <jeromegee@gmail.com>
2021-02-25 18:46:43 +0800

    Updated padding and easeOut effect

--- src/components/navbar/MobileNavLinks.js
+++ src/components/navbar/MobileNavLinks.js
@@ -1,12 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { TimelineMax, Power4 } from 'gsap';
+import { TimelineMax, Power3 } from 'gsap';
 import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const MobileNavLinks = (props) => {
 	const { isOpen } = props;
-	const [menuOpen, setMenuOpen] = useState(null);
+	const [menuOpen] = useState(null);
 
 	const timeline = new TimelineMax({ pause: true });
 	const mobile = useRef(null);
@@ -14,15 +14,15 @@ const MobileNavLinks = (props) => {
 	const showMobileLinks = () => {
 		timeline
 			.set(mobile.current, { height: 0, opacity: '0' })
-			.to(mobile.current, 1, { height: 'auto', opacity: 1, ease: Power4.easeOut }, 0.1)
+			.to(mobile.current, 1, { height: 'auto', opacity: 1, ease: Power3.easeOut }, 0.1)
 			.play();
 		return timeline;
 	};
 
 	const hideMobileLinks = () => {
 		timeline
-			.set(mobile.current, { height: 'auto', opacity: 1, ease: Power4.easeInOut })
-			.to(mobile.current, 0.5, { height: 0, opacity: 0, ease: Power4.easeOut }, 0.1)
+			.set(mobile.current, { height: 'auto', opacity: 1 })
+			.to(mobile.current, 0.5, { height: 0, opacity: 0 }, 0.1)
 			.play();
 		return timeline;
 	};
@@ -51,7 +51,6 @@ const MobileNavLinks = (props) => {
 };
 
 const mapStateToProps = (state) => {
-	console.log(state);
 	return {
 		isOpen: state.isOpen
 	};
